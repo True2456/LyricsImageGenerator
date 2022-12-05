642bb3b03617a3c08fa67972131b151f4ad087a7
TrueDebreuil <tdebreui@myune.edu.au>
2022-12-06 04:31:07 +1100

    Basic Structure

---
+++ ImageGenerator.py
@@ -0,0 +1,45 @@
+import openai as openai
+
+key = "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"
+openai.api_key = key
+image = []
+def getLyrics(input):
+    # Open the file for reading
+    with open(input, 'r') as f:
+        # Read the file contents
+        text = f.read()
+
+    # Split the file contents by lines
+    lines = text.splitlines()
+
+    # Create an empty array
+    arr = []
+
+    # Loop through the lines and add each line to the array
+    for line in lines:
+        arr.append(line)
+    return arr
+
+def generateImage(inputText):
+    response = openai.Image.create(
+      prompt= inputText,
+      n=1,
+      size="256x256"
+    )
+    return response['data'][0]['url']
+
+
+def storeImage(lyrics):
+    for i, item in enumerate(lyrics):
+        try:
+            print(item)
+            img = generateImage(item)
+            print("Image URL = " + img)
+            image.append(img)
+        except:
+            pass
+
+
+grinspoon = getLyrics('lyrics.txt')
+storeImage(grinspoon)
+print(image)
